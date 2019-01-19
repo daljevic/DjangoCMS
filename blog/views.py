bae90857234a63d8118e9e5ec053f5261e169eda
Aleksandar Daljevic <daljevic@protonmail.com>
2019-01-19 23:35:40 +0100

    templates

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Alex',
+        'title': 'First blog post',
+        'content': 'My First content',
+        'date_posted': 'August 27, 2018'
+    },
+    {
+        'author': 'Marko',
+        'title': 'Second blog post',
+        'content': 'My Second content',
+        'date_posted': 'August 28, 2018'
+    }
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html')
